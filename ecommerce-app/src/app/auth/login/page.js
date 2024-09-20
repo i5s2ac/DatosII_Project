ef2fbaa94dd8852cdbd8182df4ac9cadb661bd4a
Isaac Cyrman <Isaaccyrman@ufm.edu>
2024-09-19 22:23:01 -0600

    Update page.js

--- ecommerce-app/src/app/auth/login/page.js
+++ ecommerce-app/src/app/auth/login/page.js
@@ -13,7 +13,7 @@ export default function LoginPage() {
         e.preventDefault();
 
         try {
-            const res = await fetch('/api/login', {
+            const res = await fetch('/api/Auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
